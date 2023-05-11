ebee044dcf9851209f0ffc0195169689887e42a9
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-05-11 08:35:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Impressora.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public sealed class Impressora
+    {
+        public List<Documento> ListaDocumentos = new();
+        static Impressora instancia { get; set; }
+
+        private Impressora()
+        {
+        }
+
+        public static Impressora Instancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Impressora();
+            }
+            return instancia;
+        }
+
+        public void AdicionarImpressao(Documento documento)
+        {
+            var temIsso = ListaDocumentos.Where(item => item == documento);
+            if (!temIsso.Any())
+            {
+                ListaDocumentos.Add(documento);
+            } else
+            {
+                Console.WriteLine("Já existe esse documento na fila");
+            }
+            
+        }
+
+
+
+        public void Imprimir()
+        {
+            foreach(Documento documento in ListaDocumentos)
+            {
+                Console.WriteLine(documento.texto);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using ConsoleApp1;
+
+Impressora impressora = Impressora.Instancia();
+
+void Menu()
+{
+    bool sair = false;
+
+    while (!sair)
+    {
+        Console.WriteLine("MENU:");
+        Console.WriteLine("1 - Adicionar Documento");
+        Console.WriteLine("2 - Imprimir");
+        Console.WriteLine("3 - Sair");
+        Console.Write("Escolha uma opção: ");
+
+        string opcao = Console.ReadLine();
+
+        switch (opcao)
+        {
+            case "1":
+                Documento doc = new Documento();
+
+                Console.WriteLine("Digita ai: ");
+                doc.texto = Console.ReadLine();
+
+                impressora.AdicionarImpressao(doc);
+                break;
+
+            case "2":
+                impressora.Imprimir();
+                break;
+
+            case "3":
+                Console.Clear();
+                Console.WriteLine("Saindo...");
+                sair = true;
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida! Digite novamente.");
+                Console.ReadLine();
+                break;
+        }
+    }
+}
+
+Menu();
+
+
+
+
